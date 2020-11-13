dede0d2bf76f6e940f2633af3239868cf4ada64c
Enric <enrikk49@gmail.com>
2020-11-13 16:25:47 +0100

    Add components(Orm, annotations)

---
+++ src/Entity/Machine.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MachineRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=MachineRepository::class)
+ */
+class Machine
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
